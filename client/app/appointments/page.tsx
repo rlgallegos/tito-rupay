f1a2774a2b3ad46ece713cd449d6bc1369ef00b6
Robert <rlgallegos85@gmail.com>
2024-01-08 17:25:06 -0500

    Finished connecting mulutiple types of Calendly Events

--- client/app/appointments/page.tsx
+++ client/app/appointments/page.tsx
@@ -1,14 +1,23 @@
-import Link from "next/link"
+
 
 
 
 export default function Appointments() {
 
     return (
-      <main className="min-h-screen flex flex-col w-full bg-primary">
+      <main className="min-h-screen flex flex-col items-center justify-center w-full bg-primary">
         <div className='w-1/2 h-[100px] flex justify-evenly'>
-            <Link href='/appointments/consultation'>Consultation</Link>
-            <Link href='/appointments/meeting'>Meeting</Link>
+
+            <a className="w-1/2 border border-black flex flex-col justify-between p-6" href='/appointments/consultation'>
+                <h1>Consultation</h1>
+                <p>Book a FREE 15-min Consultation</p>
+            </a>
+
+            <a className="w-1/2 border border-black flex flex-col justify-between p-6" href='/appointments/meeting'>
+                <h1>Meeting</h1>
+                <p>Book a 60-min Session</p>
+            </a>
+            
         </div>
       </main>
     )
