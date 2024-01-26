6b9ca1922cebf89e180eda9addb4bcb444e72776
Robert <rlgallegos85@gmail.com>
2024-01-26 15:46:53 -0500

    setting up all links to contact

--- client/app/contact/page.tsx
+++ client/app/contact/page.tsx
@@ -15,21 +15,27 @@ const Contact: React.FC = () => {
 
             {/* Contact Methods */}
             <div className="border border-black w-2/3 h-1/3 flex justify-evenly z-10 bg-primary">
-                <div className="w-1/4 h-full flex flex-col justify-evenly py-12">
-                    <HiEnvelope className="h-1/4 w-full" />
-                    <h2 className="flex items-center justify-center text-xl">Email</h2>
-                    <h2 className="flex items-center justify-center text-xl">example.email@gmail.com</h2>
-                </div>
-                <div className="w-1/4 h-full flex flex-col justify-evenly py-12">
-                    <HiPhone className="h-1/4 w-full" />
-                    <h2 className="flex items-center justify-center text-xl">Call</h2>
-                    <h2 className="flex items-center justify-center text-xl">(555) 555-1234</h2>
-                </div>
-                <div className="w-1/4 h-full flex flex-col justify-evenly py-12">
-                    <HiChatBubbleLeftRight className="h-1/4 w-full" />
-                    <h2 className="flex items-center justify-center text-xl">Text</h2>
-                    <h2 className="flex items-center justify-center text-xl">(555) 555-1234</h2>
-                </div>
+                <a href="mailto:example.email@gmail.com" className="w-1/4 h-full">
+                    <div className="w-full h-full flex flex-col justify-evenly py-12">
+                        <HiEnvelope className="h-1/4 w-full" />
+                        <h2 className="flex items-center justify-center text-xl">Email</h2>
+                        <h2 className="flex items-center justify-center text-xl">example.email@gmail.com</h2>
+                    </div>
+                </a>
+                <a href="tel:+15555551234" className="w-1/4 h-full">
+                    <div className="w-full h-full flex flex-col justify-evenly py-12">
+                        <HiPhone className="h-1/4 w-full" />
+                        <h2 className="flex items-center justify-center text-xl">Call</h2>
+                        <h2 className="flex items-center justify-center text-xl">+1 (555) 555-1234</h2>
+                    </div>
+                </a>
+                <a href="smsto:+15555551234" className="w-1/4 h-full">
+                    <div className="w-full h-full flex flex-col justify-evenly py-12">
+                        <HiChatBubbleLeftRight className="h-1/4 w-full" />
+                        <h2 className="flex items-center justify-center text-xl">Text</h2>
+                        <h2 className="flex items-center justify-center text-xl">+1 (555) 555-1234</h2>
+                    </div>
+                </a>
             </div>
 
             {/* Image */}
