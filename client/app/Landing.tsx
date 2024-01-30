07ea3c28ce1d9638db38fd486ef8a77037d09b2f
Robert <rlgallegos85@gmail.com>
2024-01-30 04:20:34 -0500

    updating landing to match navbar

--- client/app/Landing.tsx
+++ client/app/Landing.tsx
@@ -4,11 +4,13 @@
 
 const Landing: React.FC = () => {
   return (
-    <div className='h-screen w-full bg-green-placeholder bg-cover object-cover flex flex-col justify-center'>
-        <div className="w-1/2 h-1/4 bg-primary text-3xl">
-            <h1>Accountability &</h1>
-            <h1>Life Coach</h1>
-            <h1>Roberto Rupay</h1>
+    <div className='h-screen w-full bg-green-placeholder bg-cover object-cover flex flex-col justify-center text-black'>
+        <div className="w-1/2 h-1/4 bg-primary text-3xl flex items-center justify-center">
+            <div className="flex flex-col">                
+                <h1>Certified</h1>
+                <h1>Accountability &</h1>
+                <h1>Life Coach</h1>
+            </div>
         </div>
     </div>
   )
--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -4,10 +4,15 @@
 
 const NavBar: React.FC = () => {
     return (
-        <nav className="h-[100px] w-full bg-primary z-10 absolute top-0 flex flex-col justify-end items-center">
+        <nav className="h-[120px] w-full bg-primary z-10 absolute top-0 flex flex-col justify-evenly items-center text-black">
 
             <h1 className="text-4xl">Roberto Rupay</h1>
-
+            <div className="w-full flex justify-evenly">
+                <p>Appointments</p>
+                <p>Services</p>
+                <p>Testimonials</p>
+                <p>Contact</p>
+            </div>
         </nav>
     )
 }
