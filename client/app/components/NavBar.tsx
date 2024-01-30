892f97cfc356ffe612ffd169f74c84e079840018
Robert <rlgallegos85@gmail.com>
2024-01-30 04:09:12 -0500

    building navbar

---
+++ client/app/components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className="h-[100px] w-full bg-primary z-10 absolute top-0 flex flex-col justify-end items-center">
+
+            <h1 className="text-4xl">Roberto Rupay</h1>
+
+        </nav>
+    )
+}
+export default NavBar
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,12 +3,14 @@ import Landing from './Landing'
 import WhoWeAre from './WhoWeAre'
 import Testimonials from './Testimonials'
 import Certifications from './Certifications'
+import NavBar from './components/NavBar'
 
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col w-full bg-primary">
+    <main className="min-h-screen flex flex-col w-full bg-primary relative">
+        <NavBar />
         <Landing />
         <div className='w-full h-screen'>
             <WhoWeAre />
