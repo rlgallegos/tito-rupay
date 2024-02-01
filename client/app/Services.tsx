45ecef8c044f398d848142688198dbfdd83b3230
Robert <rlgallegos85@gmail.com>
2024-02-01 15:44:25 -0500

    completed services section initial animations

--- client/app/Services.tsx
+++ client/app/Services.tsx
@@ -1,25 +1,51 @@
+'use client'
+
+
+
+import { useState } from "react";
 import { MdHandshake, MdAccountTree, MdApartment, MdBusinessCenter, MdWork, MdOutlineInsights, MdExtension, MdFace, MdFace3, MdFace4, MdFace6 } from "react-icons/md";
 
-
+import { useTransition, animated } from 'react-spring'
 
 const Services: React.FC = () => {
+    const [selected, setSelected] = useState<string>('')
+
+    const handleClick: (service: string) => void = (service) => {
+        if (selected === ''){
+            setSelected(service)
+        } else {
+            setSelected('')
+        }
+    }
+
+    console.log(selected)
+
     return (
         <div className="h-[66vh] w-full relative">
             <div className="absolute h-full w-full bg-forest-stone bg-center bg-cover bg-fixed"></div>
             
-            <div className="absolute w-full h-full flex justify-evenly items-center text-xl">
+            <div className="absolute w-full h-full flex justify-evenly items-center text-xl ">
 
-                <div className="w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12">
+                <div className={`w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12 transition-all duration-500
+                ${selected == 'coaching' || selected == '' ? 'opacity-1' : 'opacity-0 translate-x-4 pointer-events-none'}
+                ${selected == 'coaching' ? '' : ''} `}
+                onClick={() => handleClick('coaching')}>
                     <MdHandshake className="w-full h-1/2"/>
                     <h2 className="mt-6">1:1 Coaching</h2>
                 </div>
 
-                <div className="w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12">
+                <div className={`w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12 transition-all duration-500
+                ${selected == 'business' || selected == '' ? 'opacity-1' : 'opacity-0 translate-x-4 pointer-events-none'}
+                ${selected == 'business' ? '-translate-x-[31.25vw]' : ''}  `}
+                onClick={() => handleClick('business')}>
                     <MdBusinessCenter className="w-full h-1/2"/>
                     <h2 className="mt-6">Business Coaching</h2>
                 </div>
 
-                <div className="w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12">
+                <div className={`w-1/4 h-1/2 bg-secondary border-2 border-green-1 flex flex-col items-center justify-evenly py-12 transition-all duration-500
+                ${selected == 'group' || selected == '' ? 'opacity-1' : 'opacity-0 translate-x-4 pointer-events-none'}
+                ${selected == 'group' ? '-translate-x-[62.5vw]' : ''}  `}
+                onClick={() => handleClick('group')}>
                     <div className="w-full h-1/2 flex flex-col gap-6 items-center justify-center px-24">
                         <div className="w-full h-1/2 flex">
                             <MdFace className="w-1/2 h-full" />
--- client/app/appointments/page.tsx
+++ client/app/appointments/page.tsx
@@ -5,27 +5,35 @@ import Image from "next/image"
 export default function Appointments() {
 
     return (
-      <main className="h-screen flex items-center justify-center w-full bg-primary ">
+      <main className="h-screen flex items-center justify-center w-full bg-primary text-black">
 
+        {/* Left Half */}
         <div className=" h-full w-1/2 right-0 flex relative">
             <Image src='/images/appointments-1-transparent.png' alt="plant" layout="fill"  className=""/>
         </div>
 
-        <div className="h-full w-1/2">
+        {/* Right Half */}
+        <div className="h-full w-1/2 p-12 flex items-center justify-center">
+            <div className="h-4/5 w-4/5 bg-secondary border-2 border-green-1 flex-col">
+                <div className="w-full h-1/2 flex items-center justify-center">
+                    <h1 className="text-xl">Make <i>Your</i> Change</h1>
+                </div>
+                <div className="w-full h-1/2 flex items-center justify-center">
+                    <div className='w-4/5 flex justify-evenly bg-primary border-2 border-green-1'>
+                        <a className="w-1/2 border-r-2 border-green-1 flex flex-col p-2 gap-4" href='/appointments/consultation'>
+                            <h1>Consultation</h1>
+                            <p>Book a FREE 15-min Consultation</p>
+                        </a>
+                        <a className="w-1/2 flex flex-col p-2 gap-4" href='/appointments/meeting'>
+                            <h1>Meeting</h1>
+                            <p>Book a 60-min Session</p>
+                        </a>
+                    </div>
+                </div>
 
-            <div className='w-1/2 h-[100px] flex justify-evenly bg-primary'>
 
-                <a className="w-1/2 border border-black flex flex-col justify-between p-6" href='/appointments/consultation'>
-                    <h1>Consultation</h1>
-                    <p>Book a FREE 15-min Consultation</p>
-                </a>
-
-                <a className="w-1/2 border border-black flex flex-col justify-between p-6" href='/appointments/meeting'>
-                    <h1>Meeting</h1>
-                    <p>Book a 60-min Session</p>
-                </a>
-                
             </div>
+
         </div>
       </main>
     )
